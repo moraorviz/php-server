0a348706364cf28721cc6dd5489a7c76924cc9f7
Mario <moraorviz@gmail.com>
2018-12-01 22:58:09 +0100

    get entity http method

--- index.php
+++ index.php
@@ -9,6 +9,18 @@ require "vendor/autoload.php";
 
 $app = new App();
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+    
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->get("/php-api", function (Request $req, Response $res): Response {
     
     return $res->withJson([
@@ -30,6 +42,20 @@ $app->get('/php-api/v1', function(Request $req, Response $res, array $args): Res
     }
 );
 
+$app->get('/php-api/v1/book', function(Request $req, Response $res, array $args): Response {
+    $db = new DB('127.0.0.1', 'mariophp', 'admin', 'test');
+        $myArray = array();
+        if ($result = $db->query("SELECT * FROM books")) {
+            while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $myArray[] = $row;
+            }
+        }
+        return $res->withJson([
+            'message' => json_encode($myArray)
+        ]);
+    }
+);
+
 $app->post('/php-api/v1/book', function(Request $req, Response $res): Response {
     $body = $req->getParsedBody();
     return $res->withJson([
@@ -37,13 +63,12 @@ $app->post('/php-api/v1/book', function(Request $req, Response $res): Response {
     ]);
 });
 
-    function debug_to_console( $data ) {
-        $output = $data;
-        if ( is_array( $output ) )
-            $output = implode( ',', $output);
-            
-            echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-    }
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
 
 $app->run();
 
