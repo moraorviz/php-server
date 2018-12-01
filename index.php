b42d565dfac80b52f10269b71ebf3ed49c22e9a6
Mario <moraorviz@gmail.com>
2018-12-01 18:02:40 +0100

    get and post methods

--- index.php
+++ index.php
@@ -6,19 +6,29 @@ use Slim\Http\Response;
 require "vendor/autoload.php";
 
 $app = new App();
-$app->get("/", function (Request $req, Response $res): Response {
+
+$app->get("/php-api", function (Request $req, Response $res): Response {
     return $res->withJson([
         "message" => "Hello World!"
     ]);
 });
+
 $app->get(
-    '/users/{username}',
+    '/php-api/v1',
     function(Request $req, Response $res, array $args): Response {
         return $res->withJson([
-        'message' => 'Hello ' . $args['username']
+        'message' => 'Aqui iria ' . 'la lista de entidades '
         ]);
     }
 );
+
+$app->post('/php-api/v1/book', function(Request $req, Response $res): Response {
+    $body = $req->getParsedBody();
+    return $res->withJson([
+        'message' => 'creando book ' . $body['Title']
+    ]);
+});
+
 $app->run();
 
 
---
+++ insert_books.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (Title, Author, PublishedDate, isbn)
+VALUES ("El nido de la serpiente", "Pedro Juan Gutiérrez", "2006/01/01", "0-7583-3013-3");
