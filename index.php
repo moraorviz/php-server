2a9a70ca7483ecb39495ecf4286d1bcce71d7d40
Mario <moraorviz@gmail.com>
2018-12-02 23:35:34 +0100

    2

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $app->get('/php-api/v1/book', function(Request $req, Response $res, array $args)
             $myArray[] = $row;
         }
     }
-    var_dump($$myArray);
+    var_dump($myArray);
     return $res->withJson([
         'message' => json_encode($myArray)
     ]);
